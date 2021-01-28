b6f74066a56d646a640052980bf8c56698a38a08
Mark Brislane <G00398781@gmit.ie>
2021-01-28 19:18:21 +0000

    week 02

--- week02/calculateBMI.py
+++ week02/calculateBMI.py
@@ -2,8 +2,9 @@
 # This program is a BMI calculator. Note: no input sanitisation
 # author: Mark Brislane
 
-
+# Inputs height & weight, cast to ints so we can work with them as numbers
 height = int(input("What is your height in cm? "))
 weight = int(input("What is your weight in kg? "))
 
+# Calculate the BMI (weight / height in m2) rounded to 2 decimal places
 print ("Bad News: Your BMI is " + str(round(((weight / height / height) * 10000),2)))
