fe772dfbf592b2db7f559afead805d60b4ed1b93
Mark Brislane <g00398781@gmit.ie>
2021-02-01 17:58:10 +0000

    week 02

--- week02/calculateBMI.py
+++ week02/calculateBMI.py
@@ -7,7 +7,7 @@ while True:
     try:
         height = int(input("What is your height in cm? "))
     except ValueError:
-        print ("Sorry, I didn't understand that, please try again.")
+        print("Sorry, I didn't understand that, please try again.")
         continue
     else:
         break
@@ -15,10 +15,10 @@ while True:
     try:
         weight = int(input("What is your weight in kg? "))
     except ValueError:
-        print ("Sorry, I didn't understand that, please try again.")
+        print("Sorry, I didn't understand that, please try again.")
         continue
     else:
         break
 
 # Calculate the BMI (weight / height in m2) rounded to 2 decimal places
-print ("Bad News: Your BMI is " + str(round(((weight / height / height) * 10000),2)))
+print("Bad News: Your BMI is " + str(round(((weight / height / height) * 10000),2)))
